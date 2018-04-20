573b082be4d5a05b7690761ef863dbe413ebfd4b
Umai Z <mzhusupbekova@lcs.on.ca>
2018-04-20 12:50:34 -0400

    control+I I can write human- readable program

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -10,7 +10,7 @@ import Foundation
 
 var validInput : Int = 0 // so it can take  the value of givenInput, when we declare that validInput = givenInput
 
- while 1==1 {
+while 1==1 {
     
     // prompt the user
     print("Input number is?")
@@ -25,17 +25,17 @@ var validInput : Int = 0 // so it can take  the value of givenInput, when we dec
         continue // continue with the next iteration with while loop
         
         
-}
+    }
     
     guard let integerInput = Int (givenInput) else {
-       continue  // checking it, so only numbers will pass
+        continue  // checking it, so only numbers will pass
     }
     
     if integerInput < 1 || integerInput > 32500 {
         continue
     }
     
-validInput = integerInput
+    validInput = integerInput
     break
     
 }
@@ -47,30 +47,30 @@ var add = 1
 if  n / 1 == 1 {   // to prove that 1 is deficient as 1 cannot divide by 1. The sum is 0. value of number > sum of dividers (none or 0)
     for i in n...n/1  {
         if n % i == 0 {
-        add -= 1
+            add -= 1
         }
         break // so wont continue forever
     }
 } else {
-
-            for i in 2...n/2 {
-                
-                
-                //basically 12%2=0, 12%3=0, 12%5=2 So 1+2+3... till 6 (12%6=0).
-                if n % i == 0 {  // i is divider, n is 12 (random number more than 1 and less than 32500)
-                    
-                    add += i     // add dividers
-                   
-                    
-                }
-
-            }
+    
+    for i in 2...n/2 {
+        
+        
+        //basically 12%2=0, 12%3=0, 12%5=2 So 1+2+3... till 6 (12%6=0).
+        if n % i == 0 {  // i is divider, n is 12 (random number more than 1 and less than 32500)
             
+            add += i     // add dividers
+            
+            
+        }
+        
+    }
+    
 }
 
-        
-        
-        
+
+
+
 if n == add {
     print(n, "is a perfect")
 } else if n > add {
